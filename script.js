7c14bd32eb0255c8240e874e59fb7ada1c962640
ellafrstm <158461291+ellafrstm@users.noreply.github.com>
2024-02-12 10:00:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var gifContainer = document.getElementById("gifContainer");
+  // Visa GIF
+  gifContainer.style.display = "block";
+  // Dölj GIF efter 10 sekunder
+  setTimeout(function() {
+    gifContainer.style.display = "none";
+  }, 9800); // 10000 ms = 10 sekunder
+});
